28928352518ee474810d008eb5739dbb66c5125f
kobbyNua <kobby.nua@gmail.com>
2026-07-27 17:51:07 +0000

    backends on allow access origin

--- api/index.php
+++ api/index.php
@@ -13,8 +13,8 @@ $api=info();
 
 */
 
-
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Orign: https://omaec.com");
+//header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json, charset=UTF-8");
 header("Access-Control-Allow-Methods: POST,GET,DELETE,PUT,PATCH,OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers,Authorization,X-Requested-With");
